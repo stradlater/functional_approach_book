0832a21094e424ac179b47a2ea54d72c248b4e6c
Magnus Enerbäck <magnus.enerback@noretech.se>
2020-02-10 21:40:04 +0100

    implement vars and occurs for terms

--- chapter5/Term.fs
+++ chapter5/Term.fs
@@ -10,7 +10,7 @@ module Term =
 
         let rec parseTerms tokens level : Term<string, string> list =
             match tokens, level with
-            | Token.Ident(ident)::Token.LeftParen::rest, 0 -> 
+            | Token.Ident(ident)::Token.LeftParen::rest, 0 ->
                 let children = parseTerms rest 0
                 Term( ident, children)::(parseTerms  (Token.LeftParen::rest) 0)
             | Token.Ident(ident)::rest, 0 -> Var(ident)::(parseTerms rest 0)
@@ -29,4 +29,19 @@ module Term =
         function
         | Term(d, []) -> d + "()"
         | Term(d, t::ts) -> d + "(" + (List.fold (fun str te -> str + "," + (toString te)) (toString t) ts) + ")"
-        | Var(d) -> d
+        | Var d -> d
+
+    let rec trav f g start v =
+        function
+        | Term(d, ts) -> f(d, List.foldBack (g << (trav f g start v)) ts start)
+        | Var d -> (v d)
+
+    /// Computes the set ov variables appearing in a term
+    let vars t = trav snd Set.union Set.empty Set.singleton t
+
+    /// Tests if the given variable occurs in the given term
+    let occurs v t = 
+        let vs = vars t
+        Set.contains v vs
+
+        
--- chapter5_test/TermTest.fs
+++ chapter5_test/TermTest.fs
@@ -10,11 +10,18 @@ let ``Test ofString`` () =
     let termString = "a(b,c())"
     let term = Term.ofString termString
     Assert.Equal(termString, Term.toString term)
-    
 
 [<Fact>]
 let ``Test ofString 2`` () =
-
     let termString = "a(b(),c)"
     let term = Term.ofString termString
     Assert.Equal(termString, Term.toString term)
+
+    
+[<Fact>]
+let ``Test vars`` () =
+    let termString = "a(b(),c(c,d))"
+    let term = Term.ofString termString
+    let vars = Term.vars term
+    let expectedVars = Set.ofList(["c";"d"])
+    Assert.True( (Set.isSubset vars expectedVars) && (Set.isSubset expectedVars vars))
